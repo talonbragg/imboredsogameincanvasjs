2b140bc6d312af01228761d28d0ec0ca1e658934
Talon Bragg <pvpgamecrash@gmail.com>
2018-03-17 12:55:01 -0700

    Update game.js

--- game.js
+++ game.js
@@ -51,6 +51,30 @@ background.src = "img/Rectangle.png";
         drawRect();
         x += dx;
         y += dy;
+        function doKeyDown(evt){
+switch (evt.keyCode) {
+case 38:  /* Up arrow was pressed */
+if (y - dy > 0){
+y -= dy;
+}
+break;
+case 40:  /* Down arrow was pressed */
+if (y + dy < HEIGHT){
+y += dy;
+}
+break;
+case 37:  /* Left arrow was pressed */
+if (x - dx > 0){
+x -= dx;
+}
+break;
+case 39:  /* Right arrow was pressed */
+if (x + dx < WIDTH){
+x += dx;
+}
+break;
+}
+}
     }
     
     setInterval(draw, 10);
