41d5316b3a19b3178750d012afde50916e2581b7
Talon Bragg <pvpgamecrash@gmail.com>
2018-03-17 12:38:31 -0700

    Update game.js

--- game.js
+++ game.js
@@ -13,12 +13,12 @@ background.src = "img/Rectangle.png";
 // Make sure the image is loaded first otherwise nothing will draw.
 background.onload = function() {
 
-    setInterval(draw, 1);
+    setInterval(draw, 10);
     // Math to center the character
     x = 500 / 2 - 50 / 2;
     y = 500 / 2 - 50 / 2;
     var dx = 5;
-    var dy = 5;
+    var dy = -5;
     var WIDTH = 500;
     var HEIGHT = 500;
 
@@ -39,41 +39,14 @@ background.onload = function() {
     }
     
     function draw() {
-//     clear();    
+//  clear();    
     ctx.beginPath();
     ctx.rect(x, y, 50, 50);
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.closePath();
     }
-        function doKeyDown(evt) {
-        switch (evt.keyCode) {
-            case 38:
-                /* Up arrow was pressed */
-                if (y - dy > 0) {
-                    y -= dy;
-                }
-                break;
-            case 40:
-                /* Down arrow was pressed */
-                if (y + dy < HEIGHT) {
-                    y += dy;
-                }
-                break;
-            case 37:
-                /* Left arrow was pressed */
-                if (x - dx > 0) {
-                    x -= dx;
-                }
-                break;
-            case 39:
-                /* Right arrow was pressed */
-                if (x + dx < WIDTH) {
-                    x += dx;
-                }
-                break;
-        }
-    }
     
+    setInterval(draw, 10);
     window.addEventListener('keydown', doKeyDown, true);
 }
