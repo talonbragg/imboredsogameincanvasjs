dfeb4f69f3550296a8a396faeb9e2b22aa268fe9
Talon Bragg <pvpgamecrash@gmail.com>
2018-03-17 10:00:34 -0700

    Create game.js

---
+++ game.js
@@ -0,0 +1,8 @@
+// This creates the canvas elements and sets the size
+
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
